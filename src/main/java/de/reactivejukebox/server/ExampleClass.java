1fbf22d9751f22a8a4c41c5f286a003adbcf2ef0
Andreas Lang <andiwg@arcor.de>
2017-05-22 16:16:22 +0200

    [Project Setup] Create Jersey Example

---
+++ src/main/java/de/reactivejukebox/server/ExampleClass.java
@@ -0,0 +1,15 @@
+package de.reactivejukebox.server;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class ExampleClass {
+    @GET
+    @Path("/{param}")
+    public Response getMessage(@PathParam("param") String message) {
+        String output = "Jersey says " + message;
+        return Response.status(200).entity(output).build();
+    }
+}
