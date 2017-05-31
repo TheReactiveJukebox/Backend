2862b5a2e6b37b368b4dd8127911542d33e6e517
Figch <interfisch@gmx.net>
2017-05-31 21:33:13 +0200

    [TitleList] Add TrackService and Track Classes

---
+++ src/main/java/de/reactivejukebox/model/Track.java
@@ -0,0 +1,71 @@
+package de.reactivejukebox.model;
+
+public class Track {
+
+    int id; //global track id
+    String title;
+    String artist;
+    String album;
+    String cover;
+    int duration; //song duration in seconds
+
+    public Track(int id, String title, String artist, String album, String cover, int duration) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.cover = cover;
+        this.duration = duration;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
