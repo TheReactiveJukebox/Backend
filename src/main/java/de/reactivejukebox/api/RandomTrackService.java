f46833caa3f29be4a17fe1a5aa83cec90d2d5e5c
muesdo <dominik.mues@tu-dortmund.de>
2017-06-30 11:41:46 +0200

    [RandomTrackService] simple test database interaction

--- src/main/java/de/reactivejukebox/api/RandomTrackService.java
+++ src/main/java/de/reactivejukebox/api/RandomTrackService.java
@@ -3,6 +3,12 @@ package de.reactivejukebox.api;
 import de.reactivejukebox.core.Secured;
 import de.reactivejukebox.model.Track;
 
+import de.reactivejukebox.core.Database;
+import org.postgresql.util.PSQLException;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,17 +16,42 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Arrays;
 
+
 @Path("/randtrack")
-public class TrackService {
+public class RandomTrackService {
+    private PreparedStatement selectRandSong;
+
+
     @GET
     @Secured
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/list/{count}")
-    public Track[] getTrackList(@PathParam("count") int count) {
+    public Track[] getTrackList(@PathParam("count") int count)  throws SQLException {
+
+        // trigger loading the JDBC Driver
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();//will not happen since the driver is a working dependency
+        }
+        try {
+            /* create connection and prepare statements. Note that the Connection is never closed.
+             * This is because the connection is held until the server is shut down.
+             */
+            Connection con = Database.getInstance().getConnection();
+            selectRandSong = con.prepareStatement(
+                    "SELECT song.Id AS SongId, song.Title AS SongTitle, song.Duration AS SongDuration, song.Hash, array_agg(artist.Name) AS Artists, album.Id AS AlbumId, album.Title AS AlbumTitle, album.Cover AS AlbumCover FROM (((song LEFT JOIN song_artist ON ((song.id = song_artist.songid))) LEFT JOIN artist ON ((artist.id = song_artist.artistid))) LEFT JOIN album ON ((album.id = song.albumid))) GROUP BY song.id, song.title, song.duration, song.hash, album.id, album.title, album.cover ORDER BY RANDOM() LIMIT 1;");
+        } catch (SQLException e) {
+            throw new RuntimeException("could not establish connection to Database please restart or contact developer!");
+        }
+
+
+        Track dbRandTrack = getRandomTrackFromDB();
+
 
         Track[] trackarray = {
-                new Track(2342, "Kryptobar", "3 Foos Down", "The Better Foobar - Deluxe Edition", "https://s3.amazonaws.com/images.sheetmusicdirect.com/Album/ca8a8ef7-f305-374a-833b-b8f621ede068/large.jpg", 223),
-                new Track(25, "Kryptonite", "3 Doors Down", "The Better Life - Deluxe Edition", "https://s3.amazonaws.com/images.sheetmusicdirect.com/Album/ca8a8ef7-f305-374a-833b-b8f621ede068/large.jpg", 223),
+                new Track(2342, "Never Gonna Give You Up", "Rick Astley", "Whenever You Need Somebody", "https://lastfm-img2.akamaized.net/i/u/ar0/66055acdca0f5b29f2d89e11d837eed5", 546),
+                new Track(dbRandTrack.getId(), dbRandTrack.getTitle(), dbRandTrack.getArtist(), dbRandTrack.getAlbum(), dbRandTrack.getCover() , dbRandTrack.getDuration()),
                 new Track(23, "Hells Bells", "AC/DC", "Back in Black", "https://upload.wikimedia.org/wikipedia/en/2/23/HellsBells.jpg", 312),
                 new Track(84, "Paint it Black", "The Rolling Stones", "Singles 1965-1967", "http://www.covermesongs.com/wp-content/uploads/2010/09/PaintItBlack-400x400.jpg", 224),
                 new Track(424, "Wonderwall", "Oasis", "[What's the Story] Morning Glory?", "https://upload.wikimedia.org/wikipedia/en/1/17/Wonderwall_cover.jpg", 258),
@@ -35,5 +66,25 @@ public class TrackService {
         };
 
         return Arrays.copyOfRange(trackarray, 0, count);
+    }
+
+    public Track getRandomTrackFromDB() throws SQLException {
+        ResultSet rs = selectRandSong.executeQuery();
+        Track dbRTrack;
+        if (rs.next()) {
+            //directly fill TrackData because there can only be one row since usernames are unique
+
+            dbRTrack = new Track(
+                    rs.getInt("SongId"),
+                    rs.getString("SongTitle"),
+                    rs.getString("Artists"),
+                    rs.getString("AlbumTitle"),
+                    rs.getString("AlbumCover"),
+                    rs.getInt("SongDuration"));
+        } else {
+            throw new SQLException();
+        }
+        return dbRTrack;
+
     }
 }
