e5dac77bffff933264352e943477904794195e3c
dtext <textores.danny@gmail.com>
2017-07-11 16:20:35 +0200

    [Recommendations] Add basic structure

---
+++ src/main/java/de/reactivejukebox/recommendations/RecommendationStrategyFactory.java
@@ -0,0 +1,21 @@
+package de.reactivejukebox.recommendations;
+
+import de.reactivejukebox.recommendations.strategies.SameArtistGreatestHits;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class RecommendationStrategyFactory {
+
+    public RecommendationStrategyFactory() { // TODO add all basic parameters, e.g. User
+
+    }
+
+    public RecommendationStrategy createStrategy(String strategyName) { // TODO could also add them here
+        if (strategyName == null) {
+            throw new NotImplementedException();
+            // TODO return default strategy for user
+        } else if (strategyName.equals("sagh")) {
+            // TODO factory knows how to obtain needed0 parameters. Add algorithm parameters in constructor and provide them here
+            return new SameArtistGreatestHits();
+        } else throw new NoSuchStrategyException();
+    }
+}
---
+++ src/main/java/de/reactivejukebox/recommendations/strategies/SameArtistGreatestHits.java
@@ -0,0 +1,14 @@
+package de.reactivejukebox.recommendations.strategies;
+
+import de.reactivejukebox.model.Track;
+import de.reactivejukebox.recommendations.RecommendationStrategy;
+
+import java.util.List;
+
+public class SameArtistGreatestHits implements RecommendationStrategy{
+
+    @Override
+    public List<Track> getRecommendations() {
+        return null;
+    }
+}
