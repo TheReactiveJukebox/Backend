92893e84839071360a7bec29854fba5b48e9ea50
dtext <textores.danny@gmail.com>
2017-07-16 20:10:40 +0200

    [API] Implement API for algorithm framework
    Also remove RadioHandler class, which didn't have any function apart
    from being the class API calls are redirected to.

--- src/main/java/de/reactivejukebox/api/JukeboxService.java
+++ src/main/java/de/reactivejukebox/api/JukeboxService.java
@@ -1,17 +1,19 @@
 package de.reactivejukebox.api;
 
 import de.reactivejukebox.core.Secured;
-import de.reactivejukebox.datahandlers.RadioHandler;
-import de.reactivejukebox.model.RadioPlain;
-import de.reactivejukebox.model.TrackPlain;
-import de.reactivejukebox.model.User;
+import de.reactivejukebox.model.*;
+import de.reactivejukebox.recommendations.RecommendationStrategy;
+import de.reactivejukebox.recommendations.RecommendationStrategyFactory;
+import de.reactivejukebox.recommendations.strategies.StrategyType;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/jukebox")
 public class JukeboxService {
@@ -21,12 +23,10 @@ public class JukeboxService {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/")
     public Response getCurrentRadiostation(@Context User user) {
-
         try {
-            RadioPlain radio = new RadioHandler().getRadiostation(user);
-            return Response.ok(radio)
-                    .build();
-        }catch(SQLException e){
+            Radio radio = Model.getInstance().getRadios().getByUserId(user.getId());
+            return Response.ok(radio.getPlainObject()).build();
+        } catch (SQLException e) {
             return Response.status(503)
                     .entity("Error no Radiostation available")
                     .build();
@@ -40,10 +40,10 @@ public class JukeboxService {
     @Path("/")
     public Response createJukebox(RadioPlain r, @Context User user) {
         try {
-            RadioPlain radio = new RadioHandler().addRadiostation(r,user);
-            return Response.ok(radio)
-                    .build();
-            } catch (SQLException e) {
+            r.setUserId(user.getId());
+            Radio radio = Model.getInstance().getRadios().put(r);
+            return Response.ok(radio.getPlainObject()).build();
+        } catch (SQLException e) {
             return Response.status(503)
                     .entity("Error while writing/reading database")
                     .build();
@@ -56,13 +56,26 @@ public class JukeboxService {
     @Path("/next")
     public Response getNextSongs(@Context User user, @QueryParam("count") int count) {
         try {
-            List<TrackPlain> results = new RadioHandler().getSongs(count, user);
-            return Response.ok(results)
-                    .build();
+            Radio radio = Model.getInstance().getRadios().getByUserId(user.getId());
+            RecommendationStrategy algorithm = new RecommendationStrategyFactory(radio)
+                    .createStrategy(count);
+            List<MusicEntityPlain> results = algorithm.getRecommendations().stream()
+                    .map(Track::getPlainObject)
+                    .collect(Collectors.toList());
+            return Response.ok(results).build();
         } catch (SQLException e) {
             return Response.status(502)
                     .entity("Error while communicating with database.")
                     .build();
         }
+    }
+
+    @GET
+    @Secured
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/algorithms")
+    public Response getAlgorithms() {
+        List<String> algorithms = Arrays.stream(StrategyType.values()).map(Enum::name).collect(Collectors.toList());
+        return Response.ok(algorithms).build();
     }
 }
