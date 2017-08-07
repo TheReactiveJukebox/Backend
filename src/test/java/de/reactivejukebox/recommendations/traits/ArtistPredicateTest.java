cc3385f2f451ace08bf8f3d8f036dd9d19cc46d5
Andreas Lang <andiwg@arcor.de>
2017-08-07 13:17:31 +0200

    [FilterTests] add tests for predicates

---
+++ src/test/java/de/reactivejukebox/recommendations/traits/ArtistPredicateTest.java
@@ -0,0 +1,77 @@
+package de.reactivejukebox.recommendations.traits;
+
+import de.reactivejukebox.model.Album;
+import de.reactivejukebox.model.Artist;
+import de.reactivejukebox.model.Track;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class ArtistPredicateTest {
+
+    @Test
+    private void TestArtistPredicateTRUE() {
+        Artist a1 = new Artist(1, "test1");
+        Artist a2 = new Artist(2, "test2");
+        Track t1 = new Track(1, "Title1", a1, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t2 = new Track(2, "Title2", a2, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t3 = new Track(3, "Title3", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date());
+
+        Predicate<Track> artistPred = new ArtistPredicate(a1);
+        assertTrue(artistPred.test(t1));
+
+    }
+
+    @Test
+    private void TestArtistPredicateFALSE() {
+        Artist a1 = new Artist(1, "test1");
+        Artist a2 = new Artist(2, "test2");
+        Track t1 = new Track(1, "Title1", a1, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t2 = new Track(2, "Title2", a2, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t3 = new Track(3, "Title3", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date());
+
+        Predicate<Track> artistPred = new ArtistPredicate(a1);
+        assertFalse(artistPred.test(t2));
+
+    }
+
+    @Test
+    private void TestArtistPredicateSetTRUE() {
+        Artist a1 = new Artist(1, "test1");
+        Artist a2 = new Artist(2, "test2");
+        Track t1 = new Track(1, "Title1", a1, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t2 = new Track(2, "Title2", a2, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t3 = new Track(3, "Title3", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date());
+
+        Set<Artist> aSet = new HashSet<>();
+        aSet.add(a1);
+        aSet.add(a2);
+        Predicate<Track> artistPred = new ArtistPredicate(aSet);
+        assertTrue(artistPred.test(t1));
+        assertTrue(artistPred.test(t2));
+
+    }
+
+    @Test
+    private void TestArtistPredicateSetFALSE() {
+        Artist a1 = new Artist(1, "test1");
+        Artist a2 = new Artist(2, "test2");
+        Track t1 = new Track(1, "Title1", a1, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t2 = new Track(2, "Title2", a2, new Album(), "blacover", "blahash", 50, 0, new Date());
+        Track t3 = new Track(3, "Title3", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date());
+
+        Set<Artist> aSet = new HashSet<>();
+        aSet.add(a1);
+        aSet.add(a2);
+        Predicate<Track> artistPred = new ArtistPredicate(aSet);
+        assertFalse(artistPred.test(t3));
+    }
+
+
+}
