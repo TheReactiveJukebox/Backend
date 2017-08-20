957ed4948ad1db64f21c38791e5607cf01201185
Andreas Lang <andiwg@arcor.de>
2017-08-20 13:25:49 +0200

    [Published Test] changed to new Format

--- src/test/java/de/reactivejukebox/recommendations/filters/PublishedTest.java
+++ src/test/java/de/reactivejukebox/recommendations/filters/PublishedTest.java
@@ -5,6 +5,7 @@ import de.reactivejukebox.recommendations.strategies.StrategyType;
 import org.testng.annotations.Test;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.function.Predicate;
 
@@ -19,19 +20,16 @@ public class PublishedTest {
         radioGenres[0] = "meta1";
         Radio r = new Radio(1, new User(), radioGenres, "mood", 1990, 2000, new LinkedList<Track>(), StrategyType.RANDOM);
 
-        Date d1 = new Date();
-        d1.setYear(1990);
-        Date d2 = new Date();
-        d2.setYear(2000);
-        Date d3 = new Date();
-        d3.setYear(1989);
-        Date d4 = new Date();
-        d4.setYear(2001);
+        Date d1 = new GregorianCalendar(1990,1,1).getTime();
+        Date d2 = new GregorianCalendar(2000,1,1).getTime();
+        Date d3 = new GregorianCalendar(1989,1,1).getTime();
+        Date d4 = new GregorianCalendar(2001,1,1).getTime();
 
         Track t1 = new Track(1, "Title1", new Artist(), new Album(), "blacover", "blahash", 50, 0, d1);
         Track t2 = new Track(2, "Title2", new Artist(), new Album(), "blacover", "blahash", 50, 0, d2);
         Track t3 = new Track(1, "Title3", new Artist(), new Album(), "blacover", "blahash", 50, 0, d3);
         Track t4 = new Track(2, "Title4", new Artist(), new Album(), "blacover", "blahash", 50, 0, d4);
+        Track t5 = new Track(2, "Title5", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date());
 
         Predicate<Track> publishedPredicate = new PublishedPredicate(r);
 
@@ -39,5 +37,6 @@ public class PublishedTest {
         assertTrue(publishedPredicate.test(t2));
         assertFalse(publishedPredicate.test(t3));
         assertFalse(publishedPredicate.test(t4));
+        assertFalse(publishedPredicate.test(t5));
     }
 }
