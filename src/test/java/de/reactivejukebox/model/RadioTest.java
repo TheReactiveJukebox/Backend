95714f49aba8b42d179686bd0388a7331405e523
Andreas Lang <andiwg@arcor.de>
2017-11-11 17:07:34 +0100

    [Test] fix Genre Test

--- src/test/java/de/reactivejukebox/model/RadioTest.java
+++ src/test/java/de/reactivejukebox/model/RadioTest.java
@@ -37,7 +37,7 @@ public class RadioTest {
     private void GenreTest() {
         String[] radioGenres= new String[1];
         radioGenres[0] = "meta1";
-        Radio r = new Radio(1, new User(), radioGenres, "mood", 0, 0, 0f,0f, new LinkedList<>(), StrategyType.RANDOM);
+        Radio r = new Radio(1, new User(), radioGenres, "mood", null, null, 0f,0f, new LinkedList<>(), StrategyType.RANDOM);
 
         Track t1 = new Track(1, "Title1", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date(), 120, 0.9f);
         Track t2 = new Track(2, "Title2", new Artist(), new Album(), "blacover", "blahash", 50, 0, new Date(), 120, 0.9f);
