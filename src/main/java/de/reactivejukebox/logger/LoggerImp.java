12dc4dc8cefdaaa152c05ba6f5eb72e5684d1025
Martin Ness <m.ness@martin89.de>
2017-12-06 13:20:52 +0100

    [Logger[ Remove file handling.

--- src/main/java/de/reactivejukebox/logger/LoggerImp.java
+++ src/main/java/de/reactivejukebox/logger/LoggerImp.java
@@ -1,58 +1,23 @@
 package de.reactivejukebox.logger;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class LoggerImp implements Logger {
-    private final static String FILE_PATH = "logger/event-log.csv";
-    private final static Character DELIMITER = ';';
-
-    private static Logger instance;
-
-    private FileWriter fw;
-    private BufferedWriter bw;
 
     LoggerImp() {
-
-        try {
-            // open file in append mode
-            fw = new FileWriter(FILE_PATH, true);
-
-            bw = new BufferedWriter(fw);
-        } catch (IOException e) {
-            //exception handling left as an exercise for the reader
-            e.printStackTrace();
-        }
-
+        printHead();
     }
 
+    void printHead() {
+        String msg = "";
+        // TODO implement
+        writeEntry(msg);
     }
 
     public void writeEntry(final Entry en) throws Entry.IsInvalid {
-        if (!en.isValid()) {
-            throw new EntryIsInvalid();
-        }
-
-        StringBuilder msg = new StringBuilder();
-        for (String colValue: en.getEntry()) {
-            msg.append(colValue);
-            msg.append(DELIMITER);
-        }
-
-        writeEntry(msg.toString());
+        writeEntry(en.getLogString());
     }
 
-    private synchronized void writeEntry(final String msg) {
-        assert (!msg.trim().equals(""));
-
-        try {
-            bw.write(msg);
-            bw.newLine();
-            bw.flush();
-        } catch (IOException e) {
-            //exception handling left as an exercise for the reader
-            e.printStackTrace();
-        }
+    private void writeEntry(final String msg) {
+        // TODO log4j call
+        System.out.println(msg);
     }
 }
