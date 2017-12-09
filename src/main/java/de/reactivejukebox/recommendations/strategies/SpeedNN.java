f354d7fe32f0a97ab65cd1ed469c167a5bfcf089
kadotsu <david.brendan.spain@gmail.com>
2017-12-09 14:08:03 +0100

    [Test]Add Unit Test

--- src/main/java/de/reactivejukebox/recommendations/strategies/SpeedNN.java
+++ src/main/java/de/reactivejukebox/recommendations/strategies/SpeedNN.java
@@ -7,6 +7,7 @@ import de.reactivejukebox.recommendations.RecommendationStrategy;
 import de.reactivejukebox.recommendations.filters.HistoryPredicate;
 import de.reactivejukebox.recommendations.filters.SpeedPredicate;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -19,7 +20,7 @@ import java.util.stream.Stream;
  */
 public class SpeedNN implements RecommendationStrategy{
 
-    private static final float DefaultWindow = 10f;
+    private static final float DefaultWindow = 5f;
     private HashSet<Float> speeds;
     private Radio radio;
     private Collection<Track> upcoming;
@@ -36,11 +37,20 @@ public class SpeedNN implements RecommendationStrategy{
     };
 
     public SpeedNN(Radio radio, Collection<Track> upcoming, int resultCount){
-       this(radio,upcoming,resultCount, radio.getStartTracks().stream().map(Track::getSpeed).collect(Collectors.toSet()));
+        this(radio,upcoming,resultCount, radio.getStartTracks().stream().map(Track::getSpeed).collect(Collectors.toSet()));
     }
 
     public SpeedNN(Radio radio, Collection<Track> upcoming, int resultCount, Collection<Float> speeds){
         this(radio,upcoming,resultCount,speeds,DefaultWindow);
+    }
+
+    public SpeedNN(Radio radio, Collection<Track> upcoming, int resultCount, float minSpeed, float maxSpeed){
+        this.radio = radio;
+        this.upcoming = upcoming;
+        this.resultCount = resultCount;
+        this.window = (maxSpeed-minSpeed)/2;
+        this.speeds = new HashSet<>();
+        this.speeds.add((minSpeed+maxSpeed)/2);
     }
 
     public SpeedNN(Radio radio, Collection<Track> upcoming, int resultCount, Collection<Float> speeds,float window){
@@ -67,6 +77,15 @@ public class SpeedNN implements RecommendationStrategy{
         return radio.filter(Model.getInstance().getTracks().stream())
                 .filter(new SpeedPredicate(speed.getSpeed(),this.window))
                 .filter(new HistoryPredicate(radio,upcoming));
+    }
+
+    private List<Float> getScores(List<Track> recommendations){
+        List<Float> distances = recommendations.stream().map(Track::getSpeed).map(this::calcDistance).collect(Collectors.toList());
+        return null;
+    }
+
+    private float score(float distance){
+        return (float)Math.pow((window - distance)/window,2);
     }
 
     @Override
