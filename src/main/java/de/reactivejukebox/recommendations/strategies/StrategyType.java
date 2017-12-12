a69812ed42663727fca7b03e86c1787eddd3135f
dtext <textores.danny@gmail.com>
2017-12-12 16:05:57 +0100

    [Strategy Types] Add some logic to StrategyType enum
    This allows us to weigh algorithms differently to be able to combine
    them all together. I don't know how to weigh them; that is part of the
    tuning in real life tests.
    
    The HYBRID algorithm should always be weighed 0 in order to prevent it
    from instantiating and calling itself over and over again.

--- src/main/java/de/reactivejukebox/recommendations/strategies/StrategyType.java
+++ src/main/java/de/reactivejukebox/recommendations/strategies/StrategyType.java
@@ -1,8 +1,18 @@
 package de.reactivejukebox.recommendations.strategies;
 
 public enum StrategyType {
-    SAGH,
-    RANDOM,
-    HYBRID,
-    MOOD,
+    SAGH(5),
+    RANDOM(1),
+    HYBRID(0),
+    MOOD(4);
+
+    StrategyType(float weight) {
+        this.weight = weight;
+    }
+
+    private float weight;
+
+    public float getWeight() {
+        return weight;
+    }
 }
