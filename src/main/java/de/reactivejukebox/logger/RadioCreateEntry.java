9adf639c70b0d12b3e86655226c198ba0017a68f
Martin Ness <m.ness@martin89.de>
2017-12-13 15:59:41 +0100

    [Logger] Add RadioCreateEntry implementation

--- src/main/java/de/reactivejukebox/logger/RadioCreateEntry.java
+++ src/main/java/de/reactivejukebox/logger/RadioCreateEntry.java
@@ -1,11 +1,34 @@
 package de.reactivejukebox.logger;
 
 import de.reactivejukebox.model.Radio;
+import de.reactivejukebox.model.Track;
 import de.reactivejukebox.model.User;
+
+import java.util.StringJoiner;
 
 public class RadioCreateEntry extends Entry {
     public RadioCreateEntry(User user, Radio radio) {
         super(Event.RADIO_START, user);
-        // TODO set radio values
+        setValue(EntryCol.RADIO, radio.getId());
+        setValue(EntryCol.ALGORITHM, radio.getAlgorithm().toString());
+        setValue(EntryCol.AROUSAL, radio.getArousal());
+        setValue(EntryCol.VALENCE, radio.getValence());
+        setValue(EntryCol.DYNAMIC, radio.getDynamic());
+        setValue(EntryCol.YEAR_START, radio.getStartYear());
+        setValue(EntryCol.YEAR_END, radio.getEndYear());
+        setValue(EntryCol.SPEED_MIN, radio.getMinSpeed());
+        setValue(EntryCol.SPEED_MAX, radio.getMaxSpeed());
+
+        StringJoiner songValue = new StringJoiner(",");
+        for (Track song : radio.getStartTracks()) {
+            songValue.add(String.valueOf(song.getId()));
+        }
+        setValue(EntryCol.SONG, songValue.toString());
+
+        String[] genre = radio.getGenres();
+        if (genre != null) {
+            String genreValue = String.join(",", genre);
+            setValue(EntryCol.GENRE, genreValue);
+        }
     }
 }
