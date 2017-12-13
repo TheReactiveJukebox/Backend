c198d314835759af487ce5fa4412431f4f3f3f97
Florian Lüdiger <florian.luediger@googlemail.com>
2017-12-13 21:44:34 +0100

    [Spotify Recommender] Add option to set Spotify credentials via the Dockerfile

--- src/main/java/de/reactivejukebox/JukeboxConfig.java
+++ src/main/java/de/reactivejukebox/JukeboxConfig.java
@@ -1,8 +1,15 @@
 package de.reactivejukebox;
 
 public class JukeboxConfig {
-    public static final String spotifyClientId = "xxx";
-    public static final String spotifyClientSecret = "xxx";
+    public static final String spotifyClientId = getEnvironmentVariable("SPOTIFY_CLIENT_ID");
+    public static final String spotifyClientSecret = getEnvironmentVariable("SPOTIFY_CLIENT_SECRET");
     public static String spotifyAuthToken;
 
+    private static String getEnvironmentVariable(String variable) {
+        String value = System.getenv(variable);
+        if (value == null) {
+            throw new RuntimeException("Environment variable " + variable + " not set.");
+        }
+        return value;
+    }
 }
